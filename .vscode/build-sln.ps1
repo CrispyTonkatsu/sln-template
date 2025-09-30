10bda7a69b54ff5ba4c43bdf0801a69252c35cbb
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-09-30 11:43:03 -0700

    Added all the stuff needed for the nvim setup

---
+++ .vscode/build-sln.ps1
@@ -0,0 +1,13 @@
+param(
+	[bool]
+	$release = $false
+)
+
+& "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Launch-VsDevShell.ps1" -SkipAutomaticLocation
+
+if($release){
+	msbuild -warnaserror -p:Configuration=Release
+}else{
+	msbuild -warnaserror -p:Configuration=Debug
+}
+
