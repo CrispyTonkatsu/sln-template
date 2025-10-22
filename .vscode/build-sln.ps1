51aa1aebd0d03db91b42bc3407742ab533fa6d2a
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-10-22 10:26:14 -0700

    Fixed the release compilation target

--- .vscode/build-sln.ps1
+++ .vscode/build-sln.ps1
@@ -1,6 +1,6 @@
 param(
-	[bool]
-	$release = $false
+	[switch]
+	$release
 )
 
 & "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Launch-VsDevShell.ps1" -SkipAutomaticLocation
